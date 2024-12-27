fb6819190e4ef810d9c90f363fbffc884d8cce2f
InfernoFX <158839042+InfernoFX@users.noreply.github.com>
2024-12-27 11:00:56 +0700

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,7 +7,7 @@ use App\Models\Phone;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
-use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
+use Cloudinary\Cloudinary;
 
 
 class ProfileController extends Controller
