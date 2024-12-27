eccd0cdb231de98456fb62d707a28664e5729fbd
InfernoFX <158839042+InfernoFX@users.noreply.github.com>
2024-12-27 11:46:53 +0700

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -27,10 +27,6 @@ class LoginController extends Controller
         if ($mahasiswa && Hash::check($credentials['password'], $mahasiswa->password)) {
             Auth::guard('web')->login($mahasiswa);
             $request->session()->regenerate();
-
-            dd($mahasiswa);
-
-            dd('Password matches'); 
     
             return redirect()->intended('/homepage');
         } else {
